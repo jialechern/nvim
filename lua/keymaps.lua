38372fae0f17165a6f2082c345db19cb4642aa5f
jialechern <2692367185@qq.com>
2024-10-25 18:38:54 +0800

    增加了注释的快捷键

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,6 +15,50 @@ vim.api.nvim_set_keymap('n', '$', 'L', {noremap = true, silent = true})
                                                         -- 设置跳转到末行键
 -- vim.cmd('source $NVIMCONFIGP/lua/mathkeymaps.vim')
                                                         -- 引入数学公式的快捷键
+-- begin    设置注释
+-- 定义一个函数来添加或去除注释
+function ToggleComment()
+  local extension = vim.fn.expand('%:e')
+  if extension == 'py' then
+      comment_prefix = '# '
+  elseif extension == 'c' or 
+      extension == 'cpp' or
+      extension == 'rs'
+      then
+          comment_prefix = '// '
+  elseif extension == 'lua' then
+      comment_prefix = '-- '
+  end
+  local comment_prefix_len = #comment_prefix
+
+  -- 获取当前行号和列号
+  local line = vim.fn.line('.')
+  local col = vim.fn.col('.')
+
+  -- 获取当前行内容
+  local current_line = vim.fn.getline('.')
+
+  -- 检查当前行是否以指定的注释前缀开头
+  if current_line:find('^' .. vim.pesc(comment_prefix)) then
+    -- 如果以注释前缀开头，则移除注释
+    local new_line = current_line:gsub('^' .. vim.pesc(comment_prefix), '')
+    col = col - comment_prefix_len
+    vim.fn.setline(line, new_line)
+  else
+    -- 如果没有注释前缀，则添加注释
+    local new_line = comment_prefix .. current_line
+    col = col + comment_prefix_len
+    vim.fn.setline(line, new_line)
+  end
+
+  -- 保持光标位置
+  vim.fn.cursor(line, col)
+end
+
+vim.api.nvim_set_keymap('n', '<C-_>', ':lua ToggleComment()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-_>', '<Esc>:lua ToggleComment()<CR>a', { noremap = true, silent = true })
+
+-- end      设置注释
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown", 
@@ -36,3 +80,11 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.cmd('source $NVIMCONFIGP/lua/keymaps/keymaps-math.vim')
     end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "rust", 
+    callback = function()
+        vim.cmd('source $NVIMCONFIGP/lua/keymaps/keymaps-rust.vim')
+    end,
+})
+
