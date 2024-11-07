40eaa7a68716e9a5cf4ae6ab8241f07e67d04852
jialechern <2692367185@qq.com>
2024-11-07 15:10:12 +0800

    增加一个能够删除当前行所有传送锚点的方法

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,8 @@ vim.api.nvim_set_keymap('i', '<leader>:', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { 
 vim.api.nvim_set_keymap('i', '`;', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<C-n>', '<Esc>/<++><CR>:nohlsearch<CR>c4l', { noremap = true, silent = true })
         -- 设置写入模式的传送锚点
+vim.api.nvim_set_keymap('i', '<A-c>', '<Esc>mc:s/<++>//g<CR>`ca', { noremap = true, silent = true })
+        -- 设置清除当前行的传送锚点
 
 vim.cmd('source $NVIMCONFIGP/lua/keymaps/keymaps-base.vim')        -- 复用旧的分屏设置
 vim.api.nvim_set_keymap('n', '<leader>sc', ':set spell!<CR>', { noremap = true, silent = true})
