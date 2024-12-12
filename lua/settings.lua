c7f35999d21664eb9f9947a66550d2ea19b43e30
jialechern <2692367185@qq.com>
2024-12-12 23:27:39 +0800

    定期更新

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,7 +13,7 @@ vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a'                                     -- allow the mouse to be used in Nvim
 
 -- Tab
-vim.opt.tabstop = 8                                     -- number of visual spaces per TAB
+vim.opt.tabstop = 4                                     -- number of visual spaces per TAB
 vim.opt.softtabstop = 4                                 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4                                  -- insert 4 spaces on a tab
 vim.opt.expandtab = true                                -- tabs are spaces, mainly because of python
