211eca38ee3e071dce4287eb895cd2ebdfcefe45
jialechern <2692367185@qq.com>
2024-12-17 22:54:05 +0800

    禁用默认的启动界面

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ vim.api.nvim_set_keymap('n', '<A-r>', ':lua Run()<CR>', { noremap = true, silent
 
 -- 照例require一下，如果同时使用vim-plug插件，记得把这个放在vim-plug后面
 vim.g.mapleader = ">"   -- 需要在加载插件之前引入 leader 键
+vim.opt.shortmess:append("I")   -- 禁用默认的启动界面
 require("lazy").setup('plugins')
 
 
