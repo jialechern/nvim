581c1410c2e139a0c470397af46bf55672880312
jialechern <2692367185@qq.com>
2025-01-09 10:20:57 +0800

    完善折叠键盘映射: <C-A-.> -> z? (.->?: e->R a->M)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -90,10 +90,20 @@ vim.cmd('source $NVIMCONFIGP/lua/keymaps/keymaps-symbols.vim')
 -- end      设置成对符号的自动配对
 
 -- begin 设置代码折叠快捷键
+-- close fold
 vim.api.nvim_set_keymap('n', '<C-A-c>', 'zc', { noremap = true, silent = true })
+-- open fold
 vim.api.nvim_set_keymap('n', '<C-A-o>', 'zo', { noremap = true, silent = true })
+-- delect fold
 vim.api.nvim_set_keymap('n', '<C-A-d>', 'zd', { noremap = true, silent = true })
+-- fold
 vim.api.nvim_set_keymap('n', '<C-A-f>', 'zf', { noremap = true, silent = true })
+-- expansion 展开所有折叠
+vim.api.nvim_set_keymap('n', '<C-A-e>', 'zR', { noremap = true, silent = true })
+-- close all 关闭所有折叠
+vim.api.nvim_set_keymap('n', '<C-A-a>', 'zM', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-A-a>', 'zM', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-A-e>', 'zR', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-A-c>', 'zc', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-A-o>', 'zo', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-A-d>', 'zd', { noremap = true, silent = true })
