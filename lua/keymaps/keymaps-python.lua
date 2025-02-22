b2b3f4725d883e37ffb69c0f6573474a57e9cb2c
jialechern <2692367185@qq.com>
2025-02-22 11:33:01 +0800

    勘误

--- lua/keymaps/keymaps-python.lua
+++ lua/keymaps/keymaps-python.lua
@@ -11,7 +11,7 @@ vim.api.nvim_set_keymap('i', '<LEADER>p', 'print()<Esc>F(a', { noremap = true, s
 
 -- begin 单元测试
 -- 测试初始化(导入依赖库)
-vim.api.nvim_set_keymap('i', '<LEADER>testinit', 
+vim.api.nvim_set_keymap('i', '<LEADER>tinit', 
 'from unittest import TestCase, skip, expectedFailure, main'
 , { noremap = true, silent = true})
 -- 测试类
