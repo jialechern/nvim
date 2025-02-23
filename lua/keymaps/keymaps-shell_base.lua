8ccd8b7c782ac35ef087e057704295c9c9ac09f3
jialechern <2692367185@qq.com>
2025-02-23 23:09:45 +0800

    完善基本的 shell 操作.

--- lua/keymaps/keymaps-shell_base.lua
+++ lua/keymaps/keymaps-shell_base.lua
@@ -10,58 +10,71 @@ vim.api.nvim_set_keymap('i', '<LEADER>initz', '#!/bin/zsh<CR>', { noremap = true
 -- end init shell
 
 -- begin if 语句
+-- if
 vim.api.nvim_set_keymap('i', '<LEADER>if', 
-    'if '                                           .. '<CR>' ..
+    'if test '                                      .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'fi'                                            .. '<Esc>3kA'
 , { noremap = true, silent = true})
--- end if 语句
-
--- begin if-else 语句
+-- if-else
 vim.api.nvim_set_keymap('i', '<LEADER>eif', 
-    'if '                                           .. '<CR>' ..
+    'if test '                                      .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'else'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'fi'                                            .. '<Esc>5kA'
 , { noremap = true, silent = true})
--- end if-else 语句
-
--- begin elif 语句
+-- elif
 vim.api.nvim_set_keymap('i', '<LEADER>elif', 
-    'if '                                           .. '<CR>' ..
+    'if test '                                      .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
-    'elif ' .. _G.Next                              .. '<CR>' ..
+    'elif test ' .. _G.Next                         .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'fi'                                            .. '<Esc>6kA'
 , { noremap = true, silent = true})
--- end elif 语句
-
--- begin elif-else 语句
+-- if-elif-else 
 vim.api.nvim_set_keymap('i', '<LEADER>eelif', 
-    'if '                                           .. '<CR>' ..
+    'if test '                                      .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
-    'elif ' .. _G.Next                              .. '<CR>' ..
+    'elif test ' .. _G.Next                         .. '<CR>' ..
     'then'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'else'                                          .. '<CR>' ..
             _G.Next                                 .. '<CR>' ..
     'fi'                                            .. '<Esc>8kA'
 , { noremap = true, silent = true})
--- end elif-else 语句
+-- end if 语句
 
--- begin loop 语句
+-- begin for 循环语句
+vim.api.nvim_set_keymap('i', '<LEADER>for', 
+    'for $ in ' .. _G.Next                          .. '<CR>' ..
+    'do'                                            .. '<CR>' ..
+        _G.Next                                     .. '<CR>' ..
+    'done'                                          .. '<Esc>3k^f$a'
+, { noremap = true, silent = true})
+-- end for 循环语句
+
+-- begin loop 循环
 vim.api.nvim_set_keymap('i', '<LEADER>loop', 
     'while true'                                    .. '<CR>' ..
     'do'                                            .. '<CR>' ..
     'done'                                          .. '<Esc>O'
 , { noremap = true, silent = true})
--- end loop 语句
+-- end loop 循环
+
+-- begin while 循环
+vim.api.nvim_set_keymap('i', '<LEADER>while', 
+    'while test '                                   .. '<CR>' ..
+    'do'                                            .. '<CR>' ..
+        _G.Next                                     .. '<CR>' ..
+    'done'                                          .. '<Esc>3kA'
+, { noremap = true, silent = true})
+-- end while 循环
 
 -- begin case 分支
 vim.api.nvim_set_keymap('i', '<LEADER>case', 
@@ -73,4 +86,12 @@ vim.api.nvim_set_keymap('i', '<LEADER>case',
     'esac'                                          .. '<Esc>5k$Fihi'
 , { noremap = true, silent = true})
 -- end case 分支
+
+-- begin 函数
+vim.api.nvim_set_keymap('i', '<LEADER>func', 
+    'function () {'                                 .. '<CR>' ..
+        _G.Next                                     .. '<CR>' ..
+    '}'                                             .. '<Esc>2k^f(i'
+, { noremap = true, silent = true})
+-- end 函数
 
