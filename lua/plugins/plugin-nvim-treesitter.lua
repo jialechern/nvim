60dddfe745c21710f18c42c9943bbd3ef7e8e2dd
jialechern <2692367185@qq.com>
2025-06-04 11:18:02 +0800

    改变一些插件的配置方式.

--- lua/plugins/plugin-nvim-treesitter.lua
+++ lua/plugins/plugin-nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
-          require'nvim-treesitter.configs'.setup {
+          require('nvim-treesitter.configs').setup {
                 ensure_installed = { 
                     "bash",
                     "python",
@@ -21,6 +21,13 @@ return {
               enable = true,  -- 启用高亮
             },
           }
+
+        if vim.fn.has("termguicolors") == 1 then            -- 启用终端色彩
+            vim.opt.termguicolors = true
+        end
+   
+        vim.g.ycm_global_ycm_extra_conf = vim.fn.stdpath("config") .. '/.ycm_extra_conf.py'
+        vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
         end
       }
 }
