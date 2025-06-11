edd246a5e374a98d70d767a7ba846ce6e01f00c3
jialechern <2692367185@qq.com>
2025-06-11 10:34:25 +0800

    取消使用 firefox 预览 typst 文件, 使用默认的 chrome 预览.

--- lua/plugins/plugin-typst-preview.lua
+++ lua/plugins/plugin-typst-preview.lua
@@ -36,7 +36,8 @@ return {
       
             -- 自定义打开预览链接的命令格式，%s 会被替换为URL
             -- 示例: open_cmd = 'firefox %s -P typst-preview --class typst-preview'
-            -- open_cmd = 'firefox %s -P typst-preview --class typst-preview',
+            -- open_cmd = 'firefox %s --new-window -P typst-preview --class typst-preview',
+            open_cmd = 'google-chrome-stable --new-window %s',
       
             -- 预览服务器的端口号，0 表示随机端口
             port = 0,
