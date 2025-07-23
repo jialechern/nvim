e2b31893b9f1f4d0c86acaea337d43ccbfe413b0
jialechern <2692367185@qq.com>
2025-07-23 23:00:47 +0800

    refactor(init symbols): 去除初始化符号对原有的 loadcfg 函数的依赖, 将模块化全面改为 lua 的模块化标准.

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ else
     LoadPlugins = true
 end
 
--- begin 指定插件位置，不存在则clone到本地
+-- 指定插件位置，不存在则clone到本地
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if LoadPlugins and vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -23,17 +23,9 @@ if LoadPlugins and vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
--- end 指定插件位置，不存在则clone到本地
-
--- 引入自定义配置工具集
--- local map_by_modes = require('utils.map').map_by_modes
-local loadcfg = require('utils.loadcfg').loadcfg
-
--- 一个简单的与先前 LoadNvimConfigFile 函数的兼容措施, 不久后将会被移除
-_G.LoadNvimConfigFile = loadcfg
 
 -- 加载配置初始化时必要的符号
-loadcfg('/init/symbols.lua')
+require('settings.init-symbols')
 
 -- 设置插件管理器: 使用 Lazy.nvim 下载 CONFIG_ROOT/lua/plugins/ 下定义的插件
 if LoadPlugins then
---
+++ lua/settings/init-symbols.lua
@@ -0,0 +1,11 @@
+-- init-symbols.lua
+-- 用于配置初始化配置文件所必要的符号
+
+require('settings.init-symbols.leader')
+
+require('settings.init-symbols.coleader')
+
+require('settings.init-symbols.next')
+
+require('settings.init-symbols.end')
+
