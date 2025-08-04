56080f27a8fc846aed40913dfb3384d99c838ab7
jialechern <2692367185@qq.com>
2025-08-04 16:24:17 +0800

    refactor(snippet): 将 typst 中常用的数学代码片段使用 LuaSnip 重构.

--- snippets/typst/math.lua
+++ snippets/typst/math.lua
@@ -15,13 +15,339 @@ local c = luasnip.choice_node
 -- 模块定义
 local module = {}
 
--- module[#module+1] = s({
---     trig = 'module-math',
---     wordTrig  = true,
---     regTrig   = false,
---     -- snippetType = 'autosnippet',
--- }, {
--- })
+--- 二元关系
+
+-- 大于等于
+module[#module+1] = s({
+    trig = 'ge',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('gt.eq'),
+})
+
+-- 小于等于
+module[#module+1] = s({
+    trig = 'le',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('lt.eq'),
+})
+
+-- 不等于
+module[#module+1] = s({
+    trig = 'ne',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('eq.not'),
+})
+
+-- 等价
+module[#module+1] = s({
+    trig = 'sim',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('tilde'),
+})
+
+-- 全等于1
+module[#module+1] = s({
+    trig = 'cong',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('tilde.eq'),
+})
+
+-- 全等于2
+module[#module+1] = s({
+    trig = '==',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('equiv'),
+})
+
+-- 属于(反向)
+module[#module+1] = s({
+    trig = 'ni',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('in.rev'),
+})
+
+-- 不属于
+module[#module+1] = s({
+    trig = 'nin',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('in.not'),
+})
+
+-- 平行
+module[#module+1] = s({
+    trig = 'pl',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('parallel'),
+})
+
+-- 整除
+module[#module+1] = s({
+    trig = '|',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('divides'),
+})
+
+--- 二元运算
+
+-- 正负
+module[#module+1] = s({
+    trig = 'pm',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('plus.minus'),
+})
+
+-- 正负
+module[#module+1] = s({
+    trig = 'mp',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('minus.plus'),
+})
+
+-- 复合
+module[#module+1] = s({
+    trig = 'circ',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('compose'),
+})
+
+-- 异或/直和
+module[#module+1] = s({
+    trig = 'op',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('xor'),
+})
+
+-- 圈乘
+module[#module+1] = s({
+    trig = 'ot',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('times.circle'),
+})
+
+-- 分式
+module[#module+1] = s({
+    trig = 'frac',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('frac('), i(1), t(', '), i(2), t(')'), i(0),
+})
+
+-- 组合数
+module[#module+1] = s({
+    trig = 'C',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('binom('), i(1), t(', '), i(2), t(')'), i(0),
+})
+
+--- 积分
+
+module[#module+1] = s({
+    trig = 'integral',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('integral'),
+})
+
+module[#module+1] = s({
+    trig = 'iintegral',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('integral.double'),
+})
+
+module[#module+1] = s({
+    trig = 'iiintegral',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('integral.triple'),
+})
+
+-- 围道积分
+module[#module+1] = s({
+    trig = 'ointegral',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('integral.cont'),
+})
+
+--- 其它符号
+-- 全称量词
+module[#module+1] = s({
+    trig = 'forall',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('forall'),
+})
+
+-- 存在量词
+module[#module+1] = s({
+    trig = 'exists',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('exists'),
+})
+
+-- 无穷
+module[#module+1] = s({
+    trig = 'infty',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('infinity'),
+})
+
+-- 像
+module[#module+1] = s({
+    trig = 'im',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('im'),
+})
+
+-- 核
+module[#module+1] = s({
+    trig = 'ker',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('ker'),
+})
+
+-- 同态
+module[#module+1] = s({
+    trig = 'hom',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('hom'),
+})
+
+-- 绝对值
+module[#module+1] = s({
+    trig = 'abs',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('abs('), i(1), t(')'), i(0),
+})
+
+-- 根式
+module[#module+1] = s({
+    trig = 'sqrt',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('sqrt('), i(1), t(')'), i(0),
+})
+
+-- 范数
+module[#module+1] = s({
+    trig = 'norm',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('norm('), i(1), t(')'), i(0),
+})
+
+-- 内积
+module[#module+1] = s({
+    trig = 'inner',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('lr(angle.l '), i(1), t(' angle.r)'), i(0),
+})
+
+-- 矩阵
+module[#module+1] = s({
+    trig = 'mat',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('mat('), i(1), t(')'), i(0),
+})
+
+-- 分段函数/线性方程组
+module[#module+1] = s({
+    trig = 'cases',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('cases('), i(1), t(')'), i(0),
+})
 
 -- 模块返回
 return module
--- snippets/typst/symbols.lua
+++ snippets/typst/symbols.lua
@@ -15,13 +15,458 @@ local c = luasnip.choice_node
 -- 模块定义
 local module = {}
 
--- module[#module+1] = s({
---     trig = 'module-symbols',
---     wordTrig  = true,
---     regTrig   = false,
---     -- snippetType = 'autosnippet',
--- }, {
--- })
+--- 双线体字母
+module[#module+1] = s({
+    trig = 'bb',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('bb('), i(1), t(')'), i(0),
+})
+
+--- 花字母
+module[#module+1] = s({
+    trig = 'cal',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('cal('), i(1), t(')'), i(0),
+})
+
+--- 希腊字母
+module[#module+1] = s({
+    trig = 'alpha',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('alpha'),
+})
+
+module[#module+1] = s({
+    trig = 'beta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('beta'),
+})
+
+module[#module+1] = s({
+    trig = 'gamma',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('gamma'),
+})
+
+module[#module+1] = s({
+    trig = 'Gamma',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Gamma'),
+})
+
+module[#module+1] = s({
+    trig = 'phi',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('phi.alt'),
+})
+
+module[#module+1] = s({
+    trig = 'var-phi',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('phi'),
+})
+
+module[#module+1] = s({
+    trig = 'Phi',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Phi'),
+})
+
+module[#module+1] = s({
+    trig = 'delta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('delta'),
+})
+
+module[#module+1] = s({
+    trig = 'Delta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Delta'),
+})
+
+module[#module+1] = s({
+    trig = 'epsilon',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('epsilon.alt'),
+})
+
+module[#module+1] = s({
+    trig = 'var-epsilon',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('epsilon'),
+})
+
+module[#module+1] = s({
+    trig = 'lambda',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('lambda'),
+})
+
+module[#module+1] = s({
+    trig = 'Lambda',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Lambda'),
+})
+
+module[#module+1] = s({
+    trig = 'omega',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('omega'),
+})
+
+module[#module+1] = s({
+    trig = 'Omega',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Omega'),
+})
+
+module[#module+1] = s({
+    trig = 'sigma',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('sigma'),
+})
+
+module[#module+1] = s({
+    trig = 'Sigma',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Sigma'),
+})
+
+module[#module+1] = s({
+    trig = 'tau',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('tau'),
+})
+
+module[#module+1] = s({
+    trig = 'theta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('theta'),
+})
+
+module[#module+1] = s({
+    trig = 'Theta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('Theta'),
+})
+
+module[#module+1] = s({
+    trig = 'var-theta',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+    t('theta.alt'),
+})
+
+--- 上/下 标
+
+-- 一般性上下标
+module[#module+1] = s({
+    trig = '^',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('^('), i(1), t(')'), i(0),
+})
+
+module[#module+1] = s({
+    trig = '_',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('_('), i(1), t(')'), i(0),
+})
+
+-- 上横线
+module[#module+1] = s({
+    trig = 'overline',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('overline('), i(1), t(')'), i(0),
+})
+
+-- 下横线
+module[#module+1] = s({
+    trig = 'underline',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('underline('), i(1), t(')'), i(0),
+})
+
+-- 上括号
+module[#module+1] = s({
+    trig = 'overbrace',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('overbrace('), i(1), t(')'), i(0),
+})
+
+-- 下括号
+module[#module+1] = s({
+    trig = 'underbrace',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('underbrace('), i(1), t(')'), i(0),
+})
+
+-- 上尖括号
+module[#module+1] = s({
+    trig = 'hat',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('hat('), i(1), t(')'), i(0),
+})
+
+-- 上倒尖括号
+module[#module+1] = s({
+    trig = 'cohat',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('caron('), i(1), t(')'), i(0),
+})
+
+-- 上波浪号
+module[#module+1] = s({
+    trig = 'tilde',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('tilde('), i(1), t(')'), i(0),
+})
+
+-- 上标向右箭头
+module[#module+1] = s({
+    trig = 'arrow',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow('), i(1), t(')'), i(0),
+})
+
+--- 各种箭头
+
+-- 充要条件1
+module[#module+1] = s({
+    trig = 'Iff',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.r.double.long'),
+})
+
+-- 充要条件2
+module[#module+1] = s({
+    trig = 'sIff',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.r.double'),
+})
+
+-- 充要条件3
+module[#module+1] = s({
+    trig = 'iff',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.r.long'),
+})
+
+-- 充要条件4
+module[#module+1] = s({
+    trig = 'siff',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.r'),
+})
+
+-- 元素对应
+module[#module+1] = s({
+    trig = 'mapsto',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r.bar'),
+})
+
+-- 元素对应(加长)
+module[#module+1] = s({
+    trig = 'lmapsto',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r.long.bar'),
+})
+
+-- 必要条件1
+module[#module+1] = s({
+    trig = 'Lla',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.double.long'),
+})
+
+-- 必要条件2
+module[#module+1] = s({
+    trig = 'La',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.double'),
+})
+
+-- 必要条件3
+module[#module+1] = s({
+    trig = 'lla',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l.long'),
+})
+
+-- 必要条件4
+module[#module+1] = s({
+    trig = 'la',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.l'),
+})
+
+-- 充分条件1
+module[#module+1] = s({
+    trig = 'Lra',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r.double.long'),
+})
+
+-- 充分条件2
+module[#module+1] = s({
+    trig = 'Ra',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r.double'),
+})
+
+-- 充分条件3
+module[#module+1] = s({
+    trig = 'lra',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r.long'),
+})
+
+-- 充分条件4
+module[#module+1] = s({
+    trig = 'ra',
+    wordTrig  = true,
+    regTrig   = false,
+    -- snippetType = 'autosnippet',
+}, {
+        t('arrow.r'),
+})
 
 -- 模块返回
 return module
