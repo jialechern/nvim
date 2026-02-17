6143609d58fa9742159d022ec234348e93292ee3
jialechern <jialechern@gmail.com>
2026-02-17 23:27:02 +0800

    rebuild()
    1. 将原本能够将命令行模式下展开为当前路径的快捷键 `_G.CoLeader .. '%%'`
    变更为 `<leader>%%`;
    2. 将上述功能扩展到插入模式中.

--- lua/keymaps/base.lua
+++ lua/keymaps/base.lua
@@ -106,12 +106,12 @@ vim 内置变量快捷键说明:
     ), vim.log.levels.INFO, { title = "Vim 内置变量快捷键说明" })
 end, { desc = "显示设置 vim 内置变量的快捷键文档" })
 
--- 定义 _G.CoLeader .. '%%' 为当前文件路径
-map('c', _G.CoLeader .. '%%', function ()
+-- 定义快捷键使得其快速显示当前文件路径
+map({'i', 'c'}, '<leader>%%', function ()
     local file_path = vim.fn.expand('%:h')
     local sep = require('utils.path').path_prefix
     if file_path == '' then
-        return _G.CoLeader .. '%%'
+        return '<leader>%%'
     end
     return file_path .. sep
 end, { expr = true, desc = "在命令行中插入当前文件的路径" })
